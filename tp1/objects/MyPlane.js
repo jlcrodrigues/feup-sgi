2c08081563562ec63f4218d3fad6f2bfba832ec1
Martim Henriques <up202004421@edu.fe.up.pt>
2023-10-08 20:52:54 +0100

    fix merge

---
+++ tp1/objects/MyPlane.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+
+class MyPlane {
+  constructor(color, x=0, y=0, z=0, texturePath='') {
+    
+    const planeMaterial = new THREE.MeshPhongMaterial({
+      color: color,
+      specular: "#777777",
+      emissive: "#000000",
+      shininess: 30,
+    });
+
+    if (texturePath != ''){
+      this.planeTexture = new THREE.TextureLoader().load(texturePath);
+      this.planeTexture.wrapS = THREE.RepeatWrapping;
+      this.planeTexture.wrapT = THREE.RepeatWrapping;
+      planeMaterial.map = this.planeTexture;
+    }
+    
+    this.plane = new THREE.PlaneGeometry(10, 10);
+    this.planeMesh = new THREE.Mesh(this.plane, planeMaterial);
+    this.planeMesh.position.set(x, y, z)
+  }
+
+  getMesh() {
+    return this.planeMesh;
+  }
+}
+
+export { MyPlane };
