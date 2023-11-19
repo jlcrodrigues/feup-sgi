3637bd607057fb2add94529701b79728b3874989
luis <joseluiscunharodrigues@gmail.com>
2023-11-19 15:52:17 +0000

    Fixed material shadowing

--- tp2/builders/MyMaterialBuilder.js
+++ tp2/builders/MyMaterialBuilder.js
@@ -11,21 +11,29 @@ class MyMaterialBuilder {
    * @returns {THREE.Material} the material
    */
   static build(materialData, textures) {
-    let material = new THREE.MeshPhongMaterial({
-      color: materialData.color,
-      specular: materialData.specular,
-      emissive: materialData.emissive,
-      shininess: materialData.shininess,
-      wireframe: materialData.wireframe ?? false,
-      // TODO: shading
-      map: textures.get(materialData.textureref),
-      // TODO: texlength_s
-      // TODO: texlength_t
-      side: (materialData.twosided ?? false) ? THREE.DoubleSide : THREE.FrontSide,
-      bumpMap: textures.get(materialData.bumpref) ?? null,
-      bumpScale: materialData.bumpscale ?? null,
-      specularMap: textures.get(materialData.specularref) ?? null,
-    });
+    let material;
+    if (materialData.shading === "none") {
+      material = new THREE.MeshBasicMaterial();
+    } else {
+      material = new THREE.MeshPhongMaterial({
+        specular: materialData.specular,
+        emissive: materialData.emissive,
+        shininess: materialData.shininess,
+        // TODO: texlength_s
+        // TODO: texlength_t
+        bumpMap: textures.get(materialData.bumpref) ?? null,
+        bumpScale: materialData.bumpscale ?? null,
+        flatShading: materialData.shading === "flat",
+      });
+    }
+    material.color = materialData.color;
+    material.wireframe = materialData.wireframe ?? false;
+
+    material.map = textures.get(materialData.textureref);
+    material.side =
+      materialData.twosided ?? false ? THREE.DoubleSide : THREE.FrontSide;
+    material.specularMap = textures.get(materialData.specularref) ?? null;
+
     return material;
   }
 }
