e788704ae734e3bb8312d074c94b65d217a59c98
Martim Henriques <up202004421@edu.fe.up.pt>
2024-01-03 19:42:04 +0000

    about + tracks view

--- tp3/views/AboutView.js
+++ tp3/views/AboutView.js
@@ -1,4 +1,5 @@
 import { FontLoader } from "../loader/FontLoader.js";
+import { Fireworks } from "./Fireworks.js";
 import { View } from "./View.js";
 import * as THREE from "three";
 
@@ -15,14 +16,62 @@ class AboutView extends View {
             0.1,
             1000
         );
-        this.camera.position.z = 5;
+        this.camera.position.z = 2;
 
-        const about = new THREE.Group();
-        const aboutArray = new FontLoader().getMeshArray("KISSES FOR YOU \n esc to leave");
-        about.add(...aboutArray[0]);
-        about.scale.set(3,3,3);
-        about.position.set(-4.5,0,0)
-        this.scene.add(about);
+        this.fireworks = new Fireworks(this.scene);
+
+        const luis = new THREE.Group(); 
+        const luis1 = new THREE.Group();
+        const luis1Array = new FontLoader().getMeshArray("Luis");
+        const luis2 = new THREE.Group();
+        const luis2Array = new FontLoader().getMeshArray("Rodrigues");
+        luis1.add(...luis1Array[0]);
+        luis2.add(...luis2Array[0]);
+        luis2.position.set(0.2,-0.2,0)
+
+        luis.rotation.set(0,Math.PI/8,0);
+        luis.scale.set(5,5,0);
+        luis.position.set(-8,1,0);
+        luis.add(luis1,luis2);
+        this.scene.add(luis);
+
+        const martim = new THREE.Group(); 
+        const martim1 = new THREE.Group();
+        const martim1Array = new FontLoader().getMeshArray("Martim");
+        const martim2 = new THREE.Group();
+        const martim2Array = new FontLoader().getMeshArray("Henriques");
+        martim1.add(...martim1Array[0]);
+        martim2.add(...martim2Array[0]);
+        martim2.position.set(0.2,-0.2,0)
+
+        martim.rotation.set(0,Math.PI/8,0);
+        martim.scale.set(5,5,0);
+        martim.position.set(-8,-2,0);
+        martim.add(martim1,martim2);
+        this.scene.add(martim);
+
+        const title = new THREE.Group(); 
+        const title1 = new THREE.Group();
+        const title1Array = new FontLoader().getMeshArray("FEUP");
+        const title2 = new THREE.Group();
+        const title2Array = new FontLoader().getMeshArray("Fury GP");
+        title1.add(...title1Array[0]);
+        title2.add(...title2Array[0]);
+        title2.position.set(0.2,-0.2,0)
+
+        title.rotation.set(0,-Math.PI/8,0);
+        title.scale.set(6,6,0);
+        title.position.set(1,3,0);
+        title.add(title1,title2);
+        this.scene.add(title);
+
+        const logoTex = new THREE.TextureLoader().load("image/logo/logo.jpg");
+        const logoGeometry = new THREE.PlaneGeometry(5.5,4);
+        const logoMaterial = new THREE.MeshBasicMaterial({ color: 0xeeeeee, map: logoTex });
+        const logo = new THREE.Mesh(logoGeometry,logoMaterial);
+        logo.rotation.set(0,-Math.PI/8,0);
+        logo.position.set(4,-1.5,0);
+        this.scene.add(logo);
 
         // ambient light
         const ambientLight = new THREE.AmbientLight(0xeeeeee);
@@ -30,13 +79,19 @@ class AboutView extends View {
     }
 
     step() {
+        this.fireworks.step();
+        if (this.fireworks.dest.length%5===0) {
+            this.fireworks.launch(3, new THREE.Vector3(25,5,-70));
+            this.fireworks.launch(3, new THREE.Vector3(85,5,-70));
+        }
+
         const targetPosition = this.camera.position.clone();
-        targetPosition.x = 0;
+        targetPosition.z = 6.5;
 
         this.camera.position.lerp(targetPosition, dampingFactor);
 
         const lookAt = this.camera.position.clone();
-        this.camera.lookAt(lookAt.x*1.5, 0, 0); 
+        this.camera.lookAt(lookAt.x, 0, 0); 
     }
 }
 
--- tp3/views/TracksView.js
+++ tp3/views/TracksView.js
@@ -18,19 +18,47 @@ class TracksView extends View{
             0.1,
             1000
         );
-        this.camera.position.z = 5;
+        this.camera.position.z = 2;
+
+        const monza = new THREE.Group(); 
+        const title = new THREE.Group();
+        const titleArray = new FontLoader().getMeshArray("MONZA");
+        title.add(...titleArray[0])
+        title.scale.set(4,4,0);
+        
+        const description1 = new THREE.Group();
+        const descriptionArray = new FontLoader().getMeshArray("Iconic Italian circuit known for");
+        description1.add(...descriptionArray[0]);
+        description1.scale.set(2,2,0);
+        description1.position.set(-1,-1,0)
+        const description2 = new THREE.Group();
+        const description2Array = new FontLoader().getMeshArray("blistering straights fast corners");
+        description2.add(...description2Array[0]);
+        description2.scale.set(2,2,0);
+        description2.position.set(-1,-1.5,0)
+        const description3 = new THREE.Group();
+        const description3Array = new FontLoader().getMeshArray("and historic Formula 1 races.");
+        description3.add(...description3Array[0]);
+        description3.scale.set(2,2,0);
+        description3.position.set(-1,-2,0)
+        
+        monza.rotation.set(0,Math.PI/15,0);
+        monza.position.set(-6,1,0);
+        monza.add(title,description1,description2,description3);
+        this.scene.add(monza);
 
         const track1Tex = new THREE.TextureLoader().load( "image/tracks/monza.png" );
         const tracksPanelGeometry = new THREE.PlaneGeometry(4.5,3);
         const tracksPanelMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, map: track1Tex });
-        this.track = new THREE.Mesh(tracksPanelGeometry,tracksPanelMaterial);
-        this.track.rotation.set(0,-Math.PI/15,0);
-        this.scene.add(this.track);
+        const track = new THREE.Mesh(tracksPanelGeometry,tracksPanelMaterial);
+        track.rotation.set(0,-Math.PI/15,0);
+        track.position.set(4,0,0);
+        this.scene.add(track);
     }
 
     step(){
         const targetPosition = this.camera.position.clone();
-        targetPosition.x = -3
+        targetPosition.z = 5.5
 
         if (!App.controlsActive) {
             this.camera.position.lerp(targetPosition, dampingFactor);
